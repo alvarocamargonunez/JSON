1c6e617eb4ef81768edd9d6e0453fd4adfb8b328
alvarocamargonunez <alvaro-cnz--@hotmail.com>
2017-03-01 12:32:45 +0100

    Comienzo ejercicio 5 json

---
+++ ejercicio5json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*--
+#5) Introduce un numero de puestos determinados y muestra los mercados con ese numero de puestos.
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+import json
+from pprint import pprint
+fichero=open("/home/alvarocamargo/Descargas/segundo año/lenguaje de marcas/mercados.json","r")
+lineas=json.load(fichero)
