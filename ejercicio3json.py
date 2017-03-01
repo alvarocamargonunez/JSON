3d453ed588aa1a06473df62d3dc39b928316be49
alvarocamargonunez <alvaro-cnz--@hotmail.com>
2017-03-01 12:06:46 +0100

    FUNCIONA CORRECTAMENTE

--- ejercicio3json.py
+++ ejercicio3json.py
@@ -12,4 +12,9 @@ fichero=open("/home/alvarocamargo/Descargas/segundo año/lenguaje de marcas/merc
 lineas=json.load(fichero)
 dia=raw_input("Introduce un dia de la semana(Los días de la semana en mayúsculas):")
 for q in lineas:
+	if q["dia"]==dia :
+		print ""
+		print "MERCADOS EN EL DÍA INTRODUCIDO SON"
+		print ""
+		print "NOMBRE DE LA PEDANÍA:", q["pedania"], "----", "MERCADO NUMERO:", q["idmercado"]
 	
