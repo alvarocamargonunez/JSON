b6b6d95b579cb0db8553b01b20e472daa586dc5b
alvarocamargonunez <alvaro-cnz--@hotmail.com>
2017-03-01 11:13:38 +0100

    Comenzamos ejercicio 2 json

---
+++ ejercicio2json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*--
+#2)Programa que muestre el numero de mercados.
+import json
+with open("mercados.json","r") as fichero:
+	doc=json.load(fichero)
+
+
+
+
+
